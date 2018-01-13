2e157f772981459b2ec1f35669e99bcd852bd3cd
William Thompson <wmthmps1@gmail.com>
2018-01-13 02:12:44 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -65,9 +65,9 @@ def winner(board)
    position_2=board[win_index_2]
    position_3=board[win_index_3]
    if position_1 == "X" && position_2 == "X" && position_3 == "X"
-     return win_combination
+     return "X"
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-     return win_combination
+     return "O"
    end
   end
 end

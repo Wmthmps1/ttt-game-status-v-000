7f2593085f4adffef54ba7b1dd5fce457929780f
William Thompson <wmthmps1@gmail.com>
2018-01-13 03:00:33 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -49,7 +49,7 @@ end
 def over?(board)
 won?(board)
 full?(board)
-if won?(board) == true or full?(board) == true
+if won?(board) != false or full?(board) == true
   return true
 else
   return false
